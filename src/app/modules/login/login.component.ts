fe982413ecddce97e8a48b40f80bd0842a63aac2
Sdastider <111037073+Sdastider@users.noreply.github.com>
2022-08-14 09:09:22 +0530

    first commit

---
+++ src/app/modules/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { authService } from '../../data/service/authService.service'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: any = {};
+
+   constructor(private _service:authService,private router:Router){
+   }
+
+   ngOnInit() {
+   
+}
+
+  onSubmit(form:any){
+    let appned = `user_id=${this.user.email}&password=${this.user.password}` ;
+    this._service.getMethod(`https://retoolapi.dev/B13laa/getusers?${appned}` )
+    .subscribe(
+      res=> {
+        console.log(res)
+        console.log(Object.keys(res[0]).length)
+        //this.router.navigate(['./bodyModule'])
+         if(Object.keys(res[0]).length>1){
+           this.router.navigate(['./bodyModule'])  
+       }
+      },
+      err=> console.log(err)
+    )
+  }
+}
